2879dc86dd7878f88ed948c5d2f0416413672b9b
Samuel Elie <selie@anduril.com>
2024-12-29 15:01:38 -0800

    build pkg

--- build.config.ts
+++ build.config.ts
@@ -1,5 +1,6 @@
 import { makeConfig } from "@rad/publish/build.config.ts";
 
 export default makeConfig({
+    entries: ["src/index", "src/setup-vitest"],
     declaration: true,
 });
--- src/test.config.ts
+++ src/test.config.ts
@@ -1,59 +1,59 @@
-import { join } from "node:path";
-import { fileURLToPath } from "node:url";
-
-import { defineConfig, mergeConfig } from "vite";
 import type { InlineConfig } from "vitest";
+import { join } from "node:path";
+
+import { fileURLToPath } from "node:url";
+import { defineConfig, mergeConfig } from "vite";
 
 import { createBaseConfig } from "./base.config";
 
 const __dirname = fileURLToPath(new URL(".", import.meta.url));
 
 function getSetupFiles(): string[] {
-  const result = [join(__dirname, "./setup-vitest.ts")];
-  return result;
+    const result = [join(__dirname, "./setup-vitest.mjs")];
+    return result;
 }
 
 /**
  * Vitest config
  * Only relies on the base config
  */
-export default defineConfig((configEnv) => {
-  const baseConfig = createBaseConfig();
-  return mergeConfig(baseConfig(configEnv), {
-    resolve: {
-      // https://vitejs.dev/config/shared-options.html#resolve-conditions
-      // This tells vitest to look for the entrypoint here. Allows pkg to publish the correct fields.
-      conditions: ["vite"],
-    },
-    test: {
-      alias: [],
-      css: false,
-      reporters: ["verbose"],
-      watch: false,
-      cache: false,
-      coverage: { reportsDirectory: "public/coverage", reporter: ["html"] },
-      clearMocks: true,
-      restoreMocks: true,
-      mockReset: true,
-      passWithNoTests: true,
-      forceRerunTriggers: ["apps/**/*", "packages/**/*"],
-      isolate: true,
-      // @ts-expect-error -- not documented
-      outputTruncateLength: 600,
-      outputDiffLines: 400,
-      globals: true,
-      setupFiles: getSetupFiles(),
-      include: ["{apps,packages}/**/*.test.{js,ts,tsx}"],
-      exclude: [
-        "**/node_modules/**",
-        "**/dist/**",
-        "**/e2e/**",
-        "**/cypress/**",
-        "**/.{idea,git,output,temp}/**",
-        "**/build/**",
-        "**/public/**",
-      ],
-      environment: "jsdom",
-    } satisfies InlineConfig,
-  });
+export default defineConfig(configEnv => {
+    const baseConfig = createBaseConfig();
+    return mergeConfig(baseConfig(configEnv), {
+        resolve: {
+            // https://vitejs.dev/config/shared-options.html#resolve-conditions
+            // This tells vitest to look for the entrypoint here. Allows pkg to publish the correct fields.
+            conditions: ["vite"],
+        },
+        test: {
+            alias: [],
+            css: false,
+            reporters: ["verbose"],
+            watch: false,
+            cache: false,
+            coverage: { reportsDirectory: "public/coverage", reporter: ["html"] },
+            clearMocks: true,
+            restoreMocks: true,
+            mockReset: true,
+            passWithNoTests: true,
+            forceRerunTriggers: ["apps/**/*", "packages/**/*"],
+            isolate: true,
+            // @ts-expect-error -- not documented
+            outputTruncateLength: 600,
+            outputDiffLines: 400,
+            globals: true,
+            setupFiles: getSetupFiles(),
+            include: ["{apps,packages}/**/*.test.{js,ts,tsx}"],
+            exclude: [
+                "**/node_modules/**",
+                "**/dist/**",
+                "**/e2e/**",
+                "**/cypress/**",
+                "**/.{idea,git,output,temp}/**",
+                "**/build/**",
+                "**/public/**",
+            ],
+            environment: "jsdom",
+        } satisfies InlineConfig,
+    });
 });
