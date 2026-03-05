f64798a48b40c875b9938631faa9ad0e4b2d3917
Sam Elie <samradelie@gmail.com>
2026-03-04 19:48:56 -0800

    stuff

--- knip.config.ts
+++ knip.config.ts
@@ -5,4 +5,8 @@ import { defineKnipConfig } from "@adddog/monorepo-consistency";
 export default defineKnipConfig({
     entry: ["src/index.ts"],
     project: ["src/**/*.ts"],
+}, {
+    "ignoreDependencies": [
+        "@adddog/monorepo-consistency"
+    ]
 });
